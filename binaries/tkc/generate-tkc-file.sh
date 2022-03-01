c42f2305f1fae186f6337c9aa87e2d5d4cd59ca2
Ali Nahid <alinahid@gmail.com>
2022-03-01 15:56:13 +1100

    wip

--- binaries/tkc/generate-tkc-file.sh
+++ binaries/tkc/generate-tkc-file.sh
@@ -5,53 +5,28 @@ templateFilesDIR=$(echo "$HOME/binaries/templates" | xargs)
 
 
 source $HOME/binaries/scripts/contains-element.sh
-source $HOME/binaries/scripts/build-profile-file.sh
+source $HOME/binaries/scripts/build-deployment-file.sh
 source $HOME/binaries/scripts/extract-and-take-input.sh
 
-generateProfile () {
-    printf "\n*******Starting profile wizard*******\n\n"
+generateTKCFile () {
+    printf "\n*******Starting TKC wizard*******\n\n"
 
-    unset profilename
-    while [[ -z $profilename ]]; do
-        read -p "name of the profile: " profilename
-        if [[ -z $profilename ]]
+    unset name
+    while [[ -z $name ]]; do
+        read -p "Cluster Name: " name
+        if [[ -z $name ]]
         then
             printf "empty value is not allowed.\n"
         fi
     done
 
-    unset inp
-    unset profiletype
-    while [[ -z $profiletype ]]; do
-        read -p "type of the profile [full or lite] (default: full): " inp
-        if [[ -z $inp ]]
-        then
-            inp='full'
-        fi
-        if [[ $inp == 'full' || $inp == 'lite' ]]
-        then
-            profiletype=$inp
-        fi
-        if [[ -z $profiletype ]]
-        then
-            printf "Please provide a valid value. Only \"full\" or \"lite\" are allowed valid value.\n"
-        fi
-    done
-
-    if [[ -n $DESCRIPTOR_NAME ]]
-    then
-        DESCRIPTOR_NAME=$(echo "$DESCRIPTOR_NAME-$profiletype" | xargs)
-        printf "\nAdjusted descriptor name DESCRIPTOR_NAME=$DESCRIPTOR_NAME...ok\n"
-    fi
-
-
-    printf "\ncreating temporary file for profile...."
-    tmpProfileFile=$(echo "/tmp/profile-$profilename.yaml" | xargs)
-    cp $templateFilesDIR/profile-$profiletype.template $tmpProfileFile && printf "ok." || printf "failed"
+    printf "\ncreating temporary file...."
+    tmpTKCFile=$(echo "/tmp/tkc-$name.yaml" | xargs)
+    cp $templateFilesDIR/basetkc.template $tmpTKCFile && printf "ok." || printf "failed"
     printf "\n"
 
-    printf "generate profile file...\n"
-    buildProfileFile $tmpProfileFile
+    printf "generate TKC file...\n"
+    assembleFile $tmpProfileFile
     printf "\nprofile file generation...COMPLETE.\n"
 
 
